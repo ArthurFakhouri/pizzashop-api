d217612eacd720de287d6d4930d2374e645264a9
arthurfakhouri <arthurfakhouri@outlook.com>
2024-02-27 14:01:36 -0300

    refactor: cleaning up code and using derive to give signOut and signUser functions

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,7 +1,12 @@
 import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
-import Elysia, { t } from 'elysia'
+import Elysia, { t, type Static } from 'elysia'
 import { env } from '../env'
+
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
 
 export const auth = new Elysia()
   .use(
@@ -14,3 +19,19 @@ export const auth = new Elysia()
     }),
   )
   .use(cookie())
+  .derive(({ jwt: { sign }, setCookie, removeCookie }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await sign(payload)
+
+        setCookie('auth', token, {
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+          path: '/',
+        })
+      },
+      signOut: (cookieName: string) => {
+        removeCookie(cookieName)
+      },
+    }
+  })
